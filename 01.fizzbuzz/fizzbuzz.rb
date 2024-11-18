d3c321343ff3bf80e0912653b47add831bce10f8
kitarizum1203 <k.kitazato@genbasupport.com>
2024-11-18 13:05:16 +0900

    2回目の提出

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,14 +1,15 @@
-fizzbuzz = 0
-while fizzbuzz < 20
-  fizzbuzz += 1
-  case
-  when fizzbuzz % 3 == 0 && fizzbuzz % 5 == 0 # 3と5両方の倍数の場合
+#!/usr/bin/env ruby
+number = 0
+while number < 20
+  number += 1
+  if (number % 3 == 0) && (number % 5 == 0) # 3と5両方の倍数の場合
     puts "FizzBuzz"
-  when fizzbuzz % 3 == 0 # 3の倍数の場合
+  elsif number % 3 == 0 # 3の倍数の場合
     puts "Fizz"
-  when fizzbuzz % 5 == 0 # 5の倍数の場合
-    puts "Bizz"
+  elsif number % 5 == 0 # 5の倍数の場合
+    puts "Buzz"
   else # 上記以外の場合
-    puts fizzbuzz
+    puts number
   end
 end
+
