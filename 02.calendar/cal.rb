2abb4d2916fd87c59c43e1eb4eca84dc50e29936
kitarizum1203 <k.kitazato@genbasupport.com>
2024-12-05 15:54:19 +0900

    3回目の提出

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -2,32 +2,31 @@
 require "date"
 require "optparse"
 
-# 処理しやすいため初期化
 year = nil
 month = nil
-
-today = Date.today
 
 opt = OptionParser.new
 opt.on('-y year') {|y| year = y.to_i } 
 opt.on('-m month') {|m| month =  m.to_i }
 opt.parse!(ARGV)
 
+today = Date.today
 year ||= today.year
 month ||= today.month
 
 first_date = Date.new(year, month, 1) 
 last_date = Date.new(year, month, -1) 
 
-date_title = Date.new(year, month) 
-puts date_title.strftime("%B %Y").center(20) 
+puts first_date.strftime("%B %Y").center(20) 
 puts "Su Mo Tu We Th Fr Sa" 
 
 print "   " * first_date.wday
 
-
 (first_date..last_date).each do |date|
-  print date.day.to_s.rjust(2)
-  print "\n" if date.saturday?
-  print " " unless date.saturday?  
+  print (date.day.to_s.rjust(2))
+  if date.saturday?
+    print "\n"
+  else
+    print " "
+  end
 end
