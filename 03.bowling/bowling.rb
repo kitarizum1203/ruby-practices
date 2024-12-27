8a4ef06b34efea1fd77ac81dbe920b25f538ad4d
kitarizum1203 <k.kitazato@genbasupport.com>
2024-12-27 17:08:28 +0900

    2回目の提出

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -13,30 +13,25 @@ scores.each do |s|
   end
 end
 
-frames = []
-shots.each_slice(2) do |s|
-  frames << s
-end
+frames = shots.each_slice(2).to_a
 
 point = 0
 frames.each_with_index do |frame, i|
-  if i < 9
-    if frame[0] == 10 # strike
-      next_frame = frames[i + 1]
+  point += frame.sum
+  next if frame.sum != 10 || i >= 9
+
+  if frame[0] == 10 # strike
+    next_frame = frames[i + 1]
+    second_next_frame = frames[i + 2]
+    point +=
       if next_frame[0] == 10
-        second_next_frame = frames[i + 2]
-        point += frame[0] + next_frame[0] + second_next_frame[0]
+        next_frame[0] + second_next_frame[0]
       else
-        point += frame[0] + next_frame.sum
+        next_frame.sum
       end
-    elsif frame.sum == 10 # spare
-      next_frame = frames[i + 1]
-      point += frame.sum + next_frame[0]
-    else # other
-      point += frame.sum
-    end
-  else
-    point += frame.sum
+  elsif frame.sum == 10 # spare
+    next_frame = frames[i + 1]
+    point += next_frame[0]
   end
 end
 
